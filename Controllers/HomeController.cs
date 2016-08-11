0f4c248877138a297ba3487a5c33cf5c3131373d
Tufan Dayi <tdayi@hotmail.com>
2016-08-11 17:45:25 +0300

    v1.0.0
    upload data files and forms with AJAX

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using JqueryFileUploadWithFormData.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryFileUploadWithFormData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUser(
+            User user)
+        {
+            byte[] userPhoto = null;
+
+            if (user != null && user.File != null)
+            {
+                HttpPostedFileBase file = user.File;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    userPhoto = ms.ToArray();
+                };
+            }
+
+            this.InsertUser(
+                user.Name,
+                user.Surname,
+                user.BirthPlace,
+                userPhoto);
+
+            return Json(true);
+        }
+
+        private void InsertUser(
+            string name,
+            string surName,
+            string birthPlace,
+            byte[] photo)
+        {
+            //.......
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JqueryFileUploadWithFormData.Models
+{
+    public class User
+    {
+        public HttpPostedFileBase File { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string BirthPlace { get; set; }
+    }
+}
